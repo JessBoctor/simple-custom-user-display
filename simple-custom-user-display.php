77017d6f4e38c155c7a009c61e7631d8cfa8bb61
Jess Boctor <contactme@jessboctor.com>
2025-06-11 08:33:03 -0500

    Initial plugin file

---
+++ simple-custom-user-display.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Simple Custom User Display
+Plugin URI:  https://github.com/JessBoctor/simple-custom-user-display
+Description: Create a custom user and display in a DataView block
+Version:     0.1
+Author:      Jess Boctor
+Author URI:  http://jessboctor.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wporg
+Domain Path: /languages
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+define( 'SCUD_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
