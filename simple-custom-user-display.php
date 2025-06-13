78115e1af02c7eb0072c6d9e1b2f2f55eedb73b7
Jess Boctor <contactme@jessboctor.com>
2025-06-12 21:30:54 -0500

    Document more explicitly since this is a boilerplate

--- simple-custom-user-display.php
+++ simple-custom-user-display.php
@@ -22,12 +22,19 @@ register_activation_hook( __FILE__, 'scud_add_user_role' );
 /**
  * Register the custom user role
  * Fires only on the activation hook, so the role is only registered once
+ *
+ * To read through the documentation on adding a custom user role, see https://developer.wordpress.org/reference/functions/add_role/
+ * @param none
+ * @return void
  */
 function scud_add_user_role() {
+    // A string with no spaces to act as a slug or internal reference to the role tupe
     $role_slug = 'simple_user';
 
+    // A string which is displayed to admin screens
     $role_display_name = 'Simple User';
 
+    // WordPress Capabilities are listed here: https://wordpress.org/documentation/article/roles-and-capabilities/#capabilities
     $capabilities = [
         'read'         => true,
         'edit_posts'   => true,
