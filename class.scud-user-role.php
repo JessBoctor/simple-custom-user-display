1bcc0c620351e64707bf54197cc688d1f475aa43
Jess Boctor <contactme@jessboctor.com>
2025-06-16 18:38:07 -0500

    Avoid duplicate functionality by looping through multiple meta fields

--- class.scud-user-role.php
+++ class.scud-user-role.php
@@ -25,6 +25,18 @@ class Scud_User {
     ];
 
     /**
+     * An array containing the slug and label for meta-data fields which should be saved to these types of user
+     * The slug and label should be saved as key => value pairs:
+     * [
+     *    'meta_slug' => 'meta_label',
+     * ]
+     */
+    private array $meta_fields = [
+        'slug_1' => 'Slug 1',
+        'slug_2' => 'Slug 2',
+    ];
+
+    /**
      * Register the custom user role
      * Fires only on the activation hook, so the role is only registered once
      *
@@ -61,8 +73,6 @@ class Scud_User {
      * @return void The HTML code to disply the user field
      */
     public function display_user_profile_fields( WP_User $user ): void {
-        // Before we display the profile fields, retrieve the fields from the WP_User object
-        $saved_title = $user->get( 'scud_user_title' );
 
         // For some reason, the action hooks expect the HTML code directly and won't render a string of HTML code
         // It would be cleaner to return the HTML string, but this type of templating works too
@@ -70,12 +80,17 @@ class Scud_User {
         <h3><?php _e('Additional information'); ?></h3>
 
         <table class="form-table">
-            <tr>
-                <th><label for="scud_user_title"><?php _e( 'Title' ); ?></label></th>
-                <td>
-                    <input type="text" name="scud_user_title" id="scud_user_title" value="<?php echo esc_attr( $saved_title ); ?>" />
-                </td>
-            </tr>
+            <?php foreach ( $this->meta_fields as $field_slug => $field_label ):
+                 // Before we display the profile fields, retrieve the fields from the WP_User object
+                 $saved_meta = $user->get( 'scud_user_' . $field_slug );
+                ?>
+                    <tr>
+                        <th><label for="scud_user_<?php echo $field_slug; ?>"><?php echo $field_label; ?></label></th>
+                        <td>
+                            <input type="text" name="scud_user_<?php echo $field_slug; ?>" id="scud_user_<?php echo $field_slug; ?>" value="<?php echo esc_attr( $saved_meta ); ?>" />
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
         </table>
     <?php
     }
@@ -87,14 +102,23 @@ class Scud_User {
      * @return void;
      */
     public function save_user_profile_fields( string $user_id ): void {
-    if ( empty( $_POST['_wpnonce'] ) || ! wp_verify_nonce( $_POST['_wpnonce'], 'update-user_' . $user_id ) ) {
-        return;
+        if ( empty( $_POST['_wpnonce'] ) || ! wp_verify_nonce( $_POST['_wpnonce'], 'update-user_' . $user_id ) ) {
+            return;
+        }
+
+        if ( !current_user_can( 'edit_user', $user_id ) ) {
+            return;
+        }
+
+        $fields_not_in_post = [];
+        foreach ( $this->meta_fields as $field_slug => $field_label ) {
+            if ( array_key_exists( 'scud_user_' . $field_slug, $_POST ) ) {
+                update_user_meta( $user_id, 'scud_user_' . $field_slug, $_POST['scud_user_' . $field_slug ] );
+            } else {
+                $fields_not_in_post[] = $field_slug;
+            }
+        }
+    
     }
 
-    if ( !current_user_can( 'edit_user', $user_id ) ) {
-        return;
-    }
-
-    update_user_meta( $user_id, 'scud_user_title', $_POST['scud_user_title'] );
-    }
-}
+}   
