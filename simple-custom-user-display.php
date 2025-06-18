b93bc504c304ffcc2444330a7ce8c8f48779364a
Jess Boctor <contactme@jessboctor.com>
2025-06-18 10:06:03 -0500

    Load user groups during init hook

--- simple-custom-user-display.php
+++ simple-custom-user-display.php
@@ -13,12 +13,14 @@ Domain Path: /languages
 */
 
 use SCUD\Scud_User;
+use SCUD\Scud_Groups;
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 define( 'SCUD_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
 
 require_once SCUD_PLUGIN_DIR . 'class.scud-user-role.php';
+require_once SCUD_PLUGIN_DIR . 'class.scud-user-group.php';
 
 // Register the new User Role
 register_activation_hook( __FILE__, 'scud_activation' );
@@ -64,4 +66,18 @@ function scud_load_user(): void {
         default:
             break;
         }
+}
+
+// Register & Load the new user group taxonomies
+add_action( 'init', 'scud_load_user_groups' );
+
+// Load the taxonomies in the admin menu
+function scud_load_user_groups() {
+    $scud_groups = new Scud_Groups();
+
+    // Register taxonomies
+    $scud_groups->register_taxonomies();
+
+    // Load admin screens
+    $scud_groups->add_user_groups_to_menu();
 }
