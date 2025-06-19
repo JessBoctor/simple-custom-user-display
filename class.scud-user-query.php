282f9fd2681d46ef2844c8248e6a8c28e739afe6
Jess Boctor <contactme@jessboctor.com>
2025-06-19 09:21:11 -0500

    Avoid data breaches by only returning the information we need for the frontend

--- class.scud-user-query.php
+++ class.scud-user-query.php
@@ -27,7 +27,7 @@ if ( ! class_exists( 'Scud_User_Query' ) ) {
          * @return array Contains WP_User objects
          */
         public static function get_users(): array {
-            // Store the user data together (e.g. WP_User object, metadata, and taxonomies)
+            // Save the userdata into a single array to pass to the dataviews
             $scud_userdata = [];
 
             // Fetch all SCUD User IDs
@@ -37,19 +37,26 @@ if ( ! class_exists( 'Scud_User_Query' ) ) {
             ];
 
             $user_query = new WP_User_Query( $query_args );
-            $user_objects = $user_query->get_results();
+            $scud_user_ids = $user_query->get_results();
 
+            // We want to fetch all the meta in one call to get_user_meta
+            // But we don't need all of the meta data returned
+            // Create a filter for the returned data
             $meta_keys_for_display = array_merge( self::$meta_fields_for_display, array_keys( Scud_User::$meta_fields ) );
-            write_log( $meta_keys_for_display );
 
-            foreach ( $user_objects as $user_id ) {
+            // Go through each user ID and fetch the data we need
+            foreach ( $scud_user_ids as $user_id ) {
 
-                write_log( get_user_meta( $user_id ) );
-                // Fetch the meta data
-                $scud_userdata[ $user_id ] = array_intersect_key(
+                // Fetch the metadata and ilter to what we can use
+                $user_meta = array_intersect_key(
                     get_user_meta( $user_id ),
                     array_flip( $meta_keys_for_display )
                 );
+
+                // Fetch the user taxonomy terms
+                $user_taxonomies = [];
+
+                $scud_userdata[ $user_id ] = array_merge( $user_meta, $user_taxonomies );
             }
 
             write_log( $scud_userdata );
